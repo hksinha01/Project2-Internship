bddd471f5e1f9d3742ffe91b7b1a81b9b6a027b8
hksinha01 <hksinha19012001@gmail.com>
2022-03-26 12:44:37 +0530

    Completed

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -25,45 +25,54 @@ const intern = async function (req, res) {
               res.status(400).send({ status: false, msg: "invalid request parameters . Please Provide interns Details" })
          }
          else {
-            const { name, email, mobile} = intern;
-              if(!isValid(name))
+          let college = await collegeModel.findOne({name:intern.collegeName,isDeleted:false})
+          if(!college){
+             res.status(400).send({status : false, msg:"No Such college is Present,Please check collegeId"})}
+          
+            let internData= {
+              name : intern.name,
+              email:intern.email,
+              mobile:intern.mobile,
+              collegeId:college._id
+            }
+              if(!isValid(internData.name))
               return res.status(400).send({status: false,msg:"Enter Valid name"})
            
-              if (!isValid(email)) {
+              if (!isValid(internData.email)) {
 
                 return res.status(400).send({ status: false, message: "email is required" })
                      }
 
-            if (!(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email.trim()))) {
+            if (!(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(internData.email.trim()))) {
 
                 return res.status(400).send({ status: false, message: 'Email should be a valid email address' })
             }
-            let duplicateEmail=await internModel.findOne({email});
+            let duplicateEmail=await internModel.findOne({email : internData.email});
                 if(duplicateEmail)
                  {
                   return res.status(400).send({status:false, msg: "Email is already in use"})
                       }
               
-              if (!isValid(mobile)) {
+              if (!isValid(internData.mobile)) {
                 return res.status(400).send({ status: false, message: "mobile number is required" })
             }
 
-            if (!(/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/.test(mobile))) {
+            if (!(/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/.test(internData.mobile))) {
 
                 return res.status(400).send({ status: false, message: `Mobile Number is not valid` })
             }
-            let duplicateMobile=await internModel.findOne({mobile});
+            let duplicateMobile=await internModel.findOne({mobile :internData.mobile});
                 if(duplicateMobile)
                  {
                   return res.status(400).send({status:false, msg: "Mobile is already in use"})
                       }
         
-              let collegeId = req.body.collegeId
-              let college = await collegeModel.findById(collegeId)
-              if(!college){
-                 res.status(400).send({status : false, msg:"No Such college is Present,Please check collegeId"})}
+              // let collegeId = req.body.collegeId
+              // let college = await collegeModel.findById(collegeId)
+              // if(!college){
+              //    res.status(400).send({status : false, msg:"No Such college is Present,Please check collegeId"})}
               
-              let internCreated = await internModel.create(intern)
+              let internCreated = await internModel.create(internData)
               res.status(201).send({ status: true, data: internCreated })
          }
     }
